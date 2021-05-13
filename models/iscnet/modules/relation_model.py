aaf6b56cbbcb357d918807a09b9f5e85b96cd22f
TrungQuocNguyen <trungquoc.nguyen@tum.de>
2021-05-13 21:44:42 +0200

    Update relation module and integrate it into RfDNet

--- models/iscnet/modules/relation_model.py
+++ models/iscnet/modules/relation_model.py
@@ -2,13 +2,17 @@ import torch
 import torch.nn as nn
 import numpy as np
 from models.registers import MODULES
-
+ 
 @MODULES.register_module
 class RelationModule(nn.Module):
-    def __init__(self,n_relations = 16, appearance_feature_dim=1024,key_feature_dim = 64, geo_feature_dim = 64, isDuplication = False):
+    def __init__(self, cfg, optim_spec = None):
         super(RelationModule, self).__init__()
-        self.isDuplication=isDuplication
-        self.Nr = n_relations
+        self.cfg = cfg
+        appearance_feature_dim = cfg.config['model']['enhance_recognition']['appearance_feature_dim']
+        key_feature_dim = cfg.config['model']['enhance_recognition']['key_feature_dim']
+        geo_feature_dim = cfg.config['model']['enhance_recognition']['geo_feature_dim']
+        self.isDuplication = cfg.config['model']['enhance_recognition']['isDuplication']
+        self.Nr = cfg.config['model']['enhance_recognition']['n_relations']
         self.dim_g = geo_feature_dim
         self.relation = nn.ModuleList()
         for N in range(self.Nr):
@@ -17,12 +21,12 @@ class RelationModule(nn.Module):
         if(self.isDuplication):
             f_a, embedding_f_a, position_embedding =input_data
         else:
-            f_a, position_embedding = input_data
+            f_a, position_embedding = input_data # f_a: (B,K,appearance_feature_dim), position_embedding: (B,K,K,dim_g)
         isFirst=True
         for N in range(self.Nr):
             if(isFirst):
                 if(self.isDuplication):
-                    concat = self.relation[N](embedding_f_a,position_embedding)
+                    concat = self.relation[N](embedding_f_a,position_embedding)  #(B,K,dim_k)
                 else:
                     concat = self.relation[N](f_a,position_embedding)
                 isFirst=False
@@ -33,7 +37,7 @@ class RelationModule(nn.Module):
                     concat = torch.cat((concat, self.relation[N](f_a, position_embedding)), -1)
         return concat+f_a
 class RelationUnit(nn.Module):
-    def __init__(self, appearance_feature_dim=1024,key_feature_dim = 64, geo_feature_dim = 64):
+    def __init__(self, appearance_feature_dim=768,key_feature_dim = 96, geo_feature_dim = 96):
         super(RelationUnit, self).__init__()
         self.dim_g = geo_feature_dim
         self.dim_k = key_feature_dim
@@ -44,33 +48,31 @@ class RelationUnit(nn.Module):
         self.relu = nn.ReLU(inplace=True)
 
 
-    def forward(self, f_a, position_embedding):
-        N,_ = f_a.size()
+    def forward(self, f_a, position_embedding):#f_a: (B,K,appearance_feature_dim), position_embedding: (B,K,K,dim_g)
+        B,K,_ = f_a.size()
 
-        position_embedding = position_embedding.view(-1,self.dim_g)
+        w_g = self.relu(self.WG(position_embedding)) # (B,K,K,1)
 
-        w_g = self.relu(self.WG(position_embedding))
-        w_k = self.WK(f_a)
-        w_k = w_k.view(N,1,self.dim_k)
+        w_k = self.WK(f_a) # (B,K,dim_k)
+        w_k = w_k.view(B,K,1,self.dim_k)
 
-        w_q = self.WQ(f_a)
-        w_q = w_q.view(1,N,self.dim_k)
+        w_q = self.WQ(f_a) # (B,K,dim_k)
+        w_q = w_q.view(B,1,K,self.dim_k)
 
-        scaled_dot = torch.sum((w_k*w_q),-1 )
+        scaled_dot = torch.sum((w_k*w_q),-1 ) # (B,K,K). Note that 1st K is key, 2nd K is query 
         scaled_dot = scaled_dot / np.sqrt(self.dim_k)
 
-        w_g = w_g.view(N,N)
-        w_a = scaled_dot.view(N,N)
+        w_g = w_g.view(B,K,K) # Note that 1st K is key, 2nd K is query 
+        w_a = scaled_dot.view(B,K,K) 
 
-        w_mn = torch.log(torch.clamp(w_g, min = 1e-6)) + w_a
-        w_mn = torch.nn.Softmax(dim=1)(w_mn)
+        w_mn = torch.log(torch.clamp(w_g, min = 1e-6)) + w_a # (B,K,K)
+        w_mn = torch.nn.Softmax(dim=1)(w_mn) # compute softmax along key dimension 
 
-        w_v = self.WV(f_a)
+        w_v = self.WV(f_a) # (B,K,dim_k)
 
-        w_mn = w_mn.view(N,N,1)
-        w_v = w_v.view(N,1,-1)
+        w_mn = w_mn.view(B,K,K,1) # (B,K,K,1)
+        w_v = w_v.view(B,K,1,-1) # (B,K,1,dim_k)
+        output = w_mn*w_v # (B,K,K, dim_k)
+        output = torch.sum(output,1) # (B,K,dim_k)
 
-        output = w_mn*w_v
-
-        output = torch.sum(output,-2)
         return output
--- net_utils/relation_tool.py
+++ net_utils/relation_tool.py
@@ -58,15 +58,54 @@ def PositionalEmbedding_old( f_g, dim_g=64, wave_len=1000):
     embedding = torch.cat((sin_mat, cos_mat), -1)
 
     return embedding
-def PositionalEmbedding( f_g, dim_g=64, wave_len=1000):
+def PositionalEmbedding( f_g, dim_g=96, wave_len=1000):
     """
     Args:
         f_g: (B, K, 6)
     Returns:
         embedding: (B, K, K, dim_g)
     """
+    batch_size = f_g.shape[0]
     cx, cy, cz, l,w,h = torch.chunk(f_g, 6, dim=2) # (B,K,1)
+
+    delta_x = cx - cx.view(batch_size, 1, -1) #  (B,K,K)
+    delta_x = torch.clamp(torch.abs(delta_x / l), min=1e-3)
+    delta_x = torch.log(delta_x)
+
+    delta_y = cy - cy.view(batch_size, 1, -1)
+    delta_y = torch.clamp(torch.abs(delta_y / w), min=1e-3)
+    delta_y = torch.log(delta_y)
+
+    delta_z = cz - cz.view(batch_size, 1, -1)
+    delta_z = torch.clamp(torch.abs(delta_z / h), min=1e-3)
+    delta_z = torch.log(delta_z)
+
+    delta_l = torch.log(l.view(batch_size, 1,-1) / l)
+    delta_w = torch.log(w.view(batch_size, 1,-1) / w)
+    delta_h = torch.log(h.view(batch_size, 1,-1) / h)
+    size = delta_h.size() # (B,K,K)
+
+    delta_x = delta_x.view(size[0], size[1], size[2], 1)
+    delta_y = delta_y.view(size[0], size[1], size[2], 1)
+    delta_z = delta_z.view(size[0], size[1], size[2], 1)
+    delta_l = delta_l.view(size[0], size[1], size[2], 1)
+    delta_w = delta_w.view(size[0], size[1], size[2], 1)
+    delta_h = delta_h.view(size[0], size[1], size[2], 1)
+
+    position_mat = torch.cat((delta_x, delta_y, delta_z, delta_l, delta_w, delta_h), -1) # (B,K,K,6)
+
+    feat_range = torch.arange(dim_g / 12).cuda()
+    dim_mat = feat_range / (dim_g / 12)
+    dim_mat = 1. / (torch.pow(wave_len, dim_mat))
     
+    dim_mat = dim_mat.view(1,1, 1, 1, -1)
+    position_mat = position_mat.view(size[0], size[1], size[2], 6, -1) # (B,K,K,6,1)
+    position_mat = 100. * position_mat
+    mul_mat = position_mat * dim_mat 
 
+    mul_mat = mul_mat.view(size[0], size[1], size[2], -1) # (B,K,K,48)
+    sin_mat = torch.sin(mul_mat)
+    cos_mat = torch.cos(mul_mat)
+    embedding = torch.cat((sin_mat, cos_mat), -1) # (B,K,K,dim_g). Note that 1st K is key and 2nd K is query
 
-    return 
+    return embedding
